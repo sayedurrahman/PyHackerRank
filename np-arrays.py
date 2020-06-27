84fe37adf58ac1352b8b1b187cc19a958ad0d450
sayedurrahman <sayedur.rahmans@gmail.com>
2020-06-28 04:58:05 +0600

    np-array-done

--- np-arrays.py
+++ np-arrays.py
@@ -5,3 +5,12 @@ Created on Sun Jun 28 04:21:31 2020
 @author: Sayedur Rahman(sayedur.rahmans@gmail.com)
 """
 
+import numpy
+
+def arrays(arr):
+    np_arr= numpy.array(arr, dtype=float)
+    return numpy.flip(np_arr)
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
