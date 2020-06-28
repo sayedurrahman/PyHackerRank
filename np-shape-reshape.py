90463ed610fdd6185c2f2c3efff23dd1c5d9ea69
sayedurrahman <sayedur.rahmans@gmail.com>
2020-06-29 04:15:53 +0600

    File added

---
+++ np-shape-reshape.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 01:57:22 2020
+
+@author: Sayedur Rahman(sayedur.rahmans@gmail.com)
+Problem: 
+https://www.hackerrank.com/challenges/np-shape-reshape/problem
+"""
+
