1bc2045d14c7513dc0229c4e95959d7361c3dc7e
sayedurrahman <sayedur.rahmans@gmail.com>
2020-07-03 09:24:25 +0600

    Create np-inner-and-outer.py

---
+++ np-inner-and-outer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 06:01:09 2020
+
+@author: Sayedur Rahman(sayedur.rahmans@gmail.com)
+Problem:
+https://www.hackerrank.com/challenges/np-inner-and-outer/problem
+"""
+
+import numpy
+
+array_1=numpy.array(input().split(),dtype=int)
+array_2=numpy.array(input().split(),dtype=int)
+
+print(numpy.inner(array_1,array_2))
+print(numpy.outer(array_1,array_2))
