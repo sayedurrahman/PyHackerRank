01ba0424f1499bc7c7b645d5ff9922003483476d
sayedurrahman <sayedur.rahmans@gmail.com>
2020-07-22 14:03:44 +0600

    Create python-loops.py

---
+++ python-loops.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 22:31:28 2020
+
+@author: Sayedur Rahman(sayedur.rahmans@gmail.com)
+Problem: 
+https://www.hackerrank.com/challenges/python-loops/problem
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(n):
+    print(pow(i,2))
