debf4009a2e6b0b9b5b63b78ccabec59c6be74c0
sayedurrahman <sayedur.rahmans@gmail.com>
2020-07-22 14:04:31 +0600

    Create python_arithmetic_operators.py

---
+++ python_arithmetic_operators.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 22:25:51 2020
+
+@author: Sayedur Rahman(sayedur.rahmans@gmail.com)
+Problem: 
+https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+print(a+b)
+print(a-b)
+print(a*b)
