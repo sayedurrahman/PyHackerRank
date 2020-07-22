862d9d0df2e628decb30b1d36915f33abd884b7f
sayedurrahman <sayedur.rahmans@gmail.com>
2020-07-22 14:04:53 +0600

    Create python-print.py

---
+++ python-print.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 22:36:49 2020
+
+@author: Sayedur Rahman(sayedur.rahmans@gmail.com)
+Problem: 
+https://www.hackerrank.com/challenges/python-print/problem
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i+1,end='')
