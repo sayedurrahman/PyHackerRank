f16cbf209c9fb369a471f42f439e9206f060bf2d
sayedurrahman <sayedur.rahmans@gmail.com>
2020-07-22 14:04:09 +0600

    Create python_division.py

---
+++ python_division.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 22:28:20 2020
+
+@author: Sayedur Rahman(sayedur.rahmans@gmail.com)
+Problem: 
+https://www.hackerrank.com/challenges/
+/problem
+"""
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+print(a//b)
+print(a/b*1.0)
